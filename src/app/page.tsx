9e239f884ab5b27dc2ace52ad680ff1563f01927
Storyos <storyoser@pukyong.ac.kr>
2024-08-26 21:59:39 +0900

    [STYLE] : BottomNavBar 컴포넌트 수정 - 각 태그별 ClassName 명명 - className 별 Globals.css 재설정

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import BottomNavBar from "@/components/layouts/BottomNavBar";
+
+export default function Page() {
+    return (<>
+    <BottomNavBar></BottomNavBar>
+    </>)
+}
--- src/components/layouts/BottomNavBar.tsx
+++ src/components/layouts/BottomNavBar.tsx
@@ -2,27 +2,39 @@ import { Hamburger, Home, MyPage, Present, RecentGood } from "../icons/Index";
 
 export default function BottomNavBar() {
   return (
-    <nav className="bottom-app-nav">
-      <button className="bottom-app-button">
-        <Hamburger color="black" />
-        <span className=" bottom-app-span">카테고리</span>
-      </button>
-      <button className="bottom-app-button">
-        <Present />
-        <span className="bottom-app-span">선물하기</span>
-      </button>
-      <button className="bottom-app-button">
-        <Home />
-        <span className="bottom-app-span">홈</span>
-      </button>
-      <button className="bottom-app-button">
-        <MyPage />
-        <span className="bottom-app-span">MY</span>
-      </button>
-      <button className="bottom-app-button">
-        <RecentGood />
-        <span className="bottom-app-span">최근본</span>
-      </button>
+    <nav className="bottom-nav" aria-label="Bottom Navigation">
+      <ul className="bottom-nav-list">
+        <li className="bottom-nav-item">
+          <button className="bottom-nav-button">
+            <Hamburger color="black" />
+            <span className="bottom-nav-span">카테고리</span>
+          </button>
+        </li>
+        <li className="bottom-nav-item">
+          <button className="bottom-nav-button">
+            <Present />
+            <span className="bottom-nav-span">선물하기</span>
+          </button>
+        </li>
+        <li className="bottom-nav-item">
+          <button className="bottom-nav-button">
+            <Home />
+            <span className="bottom-nav-span">홈</span>
+          </button>
+        </li>
+        <li className="bottom-nav-item">
+          <button className="bottom-nav-button">
+            <MyPage />
+            <span className="bottom-nav-span">MY</span>
+          </button>
+        </li>
+        <li className="bottom-nav-item">
+          <button className="bottom-nav-button">
+            <RecentGood />
+            <span className="bottom-nav-span">최근본</span>
+          </button>
+        </li>
+      </ul>
     </nav>
   );
 }
