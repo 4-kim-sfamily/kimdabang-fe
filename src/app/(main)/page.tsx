9bdae40ee8254c759efb61b11cd66cda0a52baed
storyoser <storyoser@pukyong.ac.kr>
2024-08-27 14:48:19 +0900

    [ADD] : 꼬리있는 화살표 컴포넌트 추가

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import ArrowWithTail from "@/components/icons/ArrowWithTail";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <ArrowWithTail rotate_degree="90"></ArrowWithTail>
+      <ArrowWithTail rotate_degree="180"></ArrowWithTail>
+      <ArrowWithTail rotate_degree="270"></ArrowWithTail>
+    </div>
+  );
+}
