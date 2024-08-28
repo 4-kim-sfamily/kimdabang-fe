2fbb7a571252f4650a719a49c200690d7ee2a1c5
dani001024 <aj9473@naver.com>
2024-08-28 14:16:28 +0900

    [FEAT] : 베스트 리뷰 상품의 리뷰  content 추가

--- src/types/review/ReviewDataType.ts
+++ src/types/review/ReviewDataType.ts
@@ -1,10 +1,15 @@
 export interface ReviewPreviewType {
   rate: number;
   reviewCnt: number;
-  reviewPreviewImg?: reviewPreviewImgType[];
+  reviewPreviewImg?: ReviewPreviewImgType[];
 }
 
-export interface reviewPreviewImgType {
+export interface ReviewPreviewImgType {
   id: number;
   imgUrl: string;
+}
+
+//베스트 상품 리뷰(+리뷰 content)
+export interface RecentReviewPreviewType extends ReviewPreviewType {
+  recentReviewcontent: string;
 }
