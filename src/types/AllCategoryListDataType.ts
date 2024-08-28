cd2330e33102f655f7767f76589a71c300d9d515
storyoser <storyoser@pukyong.ac.kr>
2024-08-28 10:43:35 +0900

    [FEAT] : AllCategoryListDataType 타입 선언

---
+++ src/types/AllCategoryListDataType.ts
@@ -0,0 +1,12 @@
+interface Category {
+  id: number;
+  name: string;
+  parentId?: number;
+}
+
+// 전체 카테고리 데이터를 담을 타입 정의
+export interface AllCategoryListDataType {
+  mainCategories: Category[]; // 대분류
+  subCategories: Category[]; // 중분류
+  subSubCategories: Category[]; // 소분류
+}
