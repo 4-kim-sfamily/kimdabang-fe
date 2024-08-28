e413b225ca6759687db7aa2744882cd20f6226b8
storyoser <storyoser@pukyong.ac.kr>
2024-08-28 14:36:04 +0900

    [CHORE] : Category 타입 구조 변경

--- src/types/main/AllCategoryDataType.ts
+++ src/types/main/AllCategoryDataType.ts
@@ -1,17 +1,20 @@
 interface Category {
   id: number;
   name: string;
-  parentId: number;
+}
+
+interface subCategory extends Category {
+  parentId: string;
 }
 
 // 메인페이지에서 보여줄 MainCategory & 사진
-export type OnlyMainCategory = Omit<Category, "parentId"> & {
+export interface OnlyMainCategory extends Category {
   imgUrl: string;
-};
+}
 
 // 전체 카테고리 데이터를 담을 타입 정의
 export interface AllCategoryDataType {
   mainCategories: Category[]; // 대분류
-  subCategories: Category[]; // 중분류
-  detailCategories: Category[]; // 소분류
+  subCategories: subCategory[]; // 중분류
+  detailCategories: subCategory[]; // 소분류
 }
