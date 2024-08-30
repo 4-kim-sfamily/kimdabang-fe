d0a9f458edac408c36804ae8f2e5cc050d3346fb
storyoser <storyoser@pukyong.ac.kr>
2024-08-30 10:52:33 +0900

    [FEAT] : 스타벅스 아이콘 cn으로 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import StarbucksLogo from "@/components/icons/StarbucksLogo";
+import { Button } from "@/components/ui/button";
 
 export default function page() {
   return (
-    <div className="w-full">
-      <StarbucksLogo></StarbucksLogo>
-      <p className="font-NanumSquare font-bold">다람쥐</p>
+    <div className="w-full ">
+      {/* <StarbucksIcon></StarbucksIcon> */}
+      <div className="text-center">
+        <Button variant="starbucks">로그인하기</Button>
+        <Button variant="starbucks" size="sm">
+          선물하기 ㅇㅇ
+        </Button>
+        <Button variant="inversion" size="sm">
+          구매하기 ㅇㅇ
+        </Button>
+      </div>
+
+      <div className="flex flex-col">
+        <button className=" mb-2 mx-6 bg-[#01a862] text-white py-3 rounded-full font-bold">
+          로그인하기
+        </button>
+      </div>
     </div>
   );
 }
