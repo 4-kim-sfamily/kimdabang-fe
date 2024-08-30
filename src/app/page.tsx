2d5eaba1ab36605d25c2fc55220012fc1e90a9c2
storyoser <storyoser@pukyong.ac.kr>
2024-08-30 12:22:38 +0900

    [FEAT]: Button sm 사이즈 추가 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,20 @@ export default function page() {
       <Button variant="kakao">카카오 하이</Button>
       <Button variant="disabled">결제 및 선물하기</Button>
       <div className="justify-around">
-        <Button variant="starbucks" size="sm">
+        <Button variant="starbucks" size="md">
           <div className="pr-2">
             <Present></Present>
           </div>
           선물하기
         </Button>
-        <Button variant="inversion" size="sm">
+        <Button variant="inversion" size="md">
           <div className="pr-2">
             <Cart color="#01a862" />
           </div>
           <span>구매하기</span>
+        </Button>
+        <Button variant="inversion" size="sm" className="text-lg">
+          주소검색
         </Button>
       </div>
     </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,16 +10,16 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        kakao: "bg-[#FEE500] text-black font-bold",
-        disabled: "bg-gray-300 text-white font-bold",
-        starbucks: "bg-[#01a862] font-bold text-white",
-        inversion:
-          "bg-white text-[#01a862] border-[#01a862] border-2 font-bold",
+        kakao: "bg-[#FEE500] text-black ",
+        disabled: "bg-gray-300 text-white ",
+        starbucks: "bg-[#01a862]  text-white",
+        inversion: "bg-white text-[#01a862] border-[#01a862] border-2 ",
       },
       size: {
-        default: "w-[88%] mb-2 py-3",
-        sm: "w-[45%] mb-2 px-8 py-3",
-        lg: "mb-2 mx-6 py-3",
+        default: "w-[88%] mb-2 py-3 font-bold",
+        sm: "w-[30%] py-2 mb-2 font-normal",
+        md: "w-[45%] mb-2 px-8 py-3 font-bold",
+        lg: "mb-2 mx-6 py-3 font-bold",
       },
     },
     defaultVariants: {
