04ab9b2ffde88a284fa6e90cea40413207b741e3
storyoser <storyoser@pukyong.ac.kr>
2024-09-01 23:31:10 +0900

    [FEAT] : AuthProvider 추가 (#18)

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,10 @@
+import { SessionProvider } from "next-auth/react";
+
+export default function AUthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <SessionProvider> {children}</SessionProvider>;
+}
+//  이거 app / layout에 넣으면 인증되고 시작됨.
