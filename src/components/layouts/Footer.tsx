881b76bb84e7922b40136262b9732307a23efd1b
storyoser <storyoser@pukyong.ac.kr>
2024-09-02 12:22:10 +0900

    [CHORE] : footer 텍스트 일부 수정

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -8,9 +8,9 @@ export default function Footer() {
           <div className="text-white">
             <CustomerServiceIcon />
             <h4 className="text-[11px] font-bold">
-              SSG.COM 고객센터 / 전자금융거래 분쟁처리
+              SPHAROS.COM 고객센터 / 전자금융거래 분쟁처리
             </h4>
-            <p>1577-3419 / ssg@ssg.com</p>
+            <p>1500-0000 / SPHAROS@SPHAROS.com</p>
           </div>
           <div className=" text-white">
             <button className="bg-gray-600 mb-2 mx-2 px-4 py-2 rounded">
@@ -46,7 +46,7 @@ export default function Footer() {
           <div className="text-[10px] mt-2">
             <p className="text-[11px]">㈜스파로스</p>
             <address className="text-[#888888]">
-              <p>대표자: 최훈학 | 사업자등록번호: 870-88-01143 </p>
+              <p>대표자: 김남우 | 사업자등록번호: 000-00-00000 </p>
               <p>
                 통신판매업 신고번호: xxxxxxxx
                 <a
@@ -56,13 +56,13 @@ export default function Footer() {
                   사업자 정보확인
                 </a>
               </p>
-              <p>개인정보보호 책임자: 김우진 | 주소: 부산 해운대구 우동</p>
+              <p>개인정보보호 책임자: 박상언 | 주소: 부산 해운대구 우동</p>
               <p>호스팅서비스 사업자 : ㈜스파로스닷컴</p>
             </address>
           </div>
           <div className="mt-6 text-[10px]">
             <p>
-              우리은행 채무지급보증 안내
+              OO은행 채무지급보증 안내
               <a href="#" className="ml-1 text-black underline hover:font-bold">
                 서비스가입사실 확인
               </a>
@@ -99,16 +99,16 @@ export default function Footer() {
           </div>
           <div className="mt-3 text-[9px]">
             <p>
-              ㈜에스에스지닷컴은 SSG.COM 실시간 항공권 서비스의
+              ㈜스파로스닷컴 SPHAROS.COM 실시간 항공권 서비스의
               통신판매중개자로서 거래 당사자가 아니며, 입점 판매사가 등록한 상품
               정보 및 거래에 대해 책임을 지지 않습니다.
             </p>
             <p>
-              ㈜에스에스지닷컴 사이트의 상품/판매자/쇼핑정보, 컨텐츠, UI 등에
-              대한 무단 복제, 전송, 배포, 스크래핑 등의 행위는 저작권법,
-              콘텐츠사업 진흥법 등에 의하여 엄격히 금지됩니다.
+              ㈜스파로스닷컴 사이트의 상품/판매자/쇼핑정보, 컨텐츠, UI 등에 대한
+              무단 복제, 전송, 배포, 스크래핑 등의 행위는 저작권법, 콘텐츠사업
+              진흥법 등에 의하여 엄격히 금지됩니다.
             </p>
-            <p className="mt-1">© SSG.COM Corp. All rights reserved.</p>
+            <p className="mt-1">© SPHAROS.COM Corp. All rights reserved.</p>
           </div>
         </div>
       </div>
