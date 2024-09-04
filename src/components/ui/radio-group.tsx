18ae1beb5758f3d4d24d5d2959d889cc8e9e8b77
dani001024 <aj9473@naver.com>
2024-09-04 15:44:37 +0900

    [FEAT] : OptionDialog 컴포넌트 퍼블리싱

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -28,13 +28,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        "aspect-square  h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:border-[#FF5452] data-[state=checked]:border-[#FF5452] focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-[#c1706e]",
         className,
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-2.5 w-2.5 fill-current text-current" />
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center data-[state=checked]:border-[#FF5452]">
+        <Circle className="h-2.5 w-2.5 fill-[#FF5452] stroke-none text-current" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   );
