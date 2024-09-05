5a2c39ce1763497deaa05b4abf5d44d5c07a83da
dani001024 <aj9473@naver.com>
2024-09-05 16:54:20 +0900

    [FEAT] : main 페이지 캐로셀 컴포넌트 퍼블리싱

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,11 +5,16 @@ import MainCategory from "@/components/pages/main/MainCategory";
 import MainGiftProduct from "@/components/pages/main/MainGiftProduct";
 import MainSeason from "@/components/pages/main/MainSeason";
 import MainCarousel from "@/components/ui/MainCarousel";
+import { CarouselDataType } from "@/types/main/CarouselDataType";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://localhost:4000/carouselData", {
+    cache: "no-store",
+  });
+  const carouselData: CarouselDataType[] = await res.json();
   return (
-    <main className="w-full">
-      <MainCarousel />
+    <main className="w-[100%]">
+      <MainCarousel carouselData={carouselData} />
       <div className="px-[4.1vw]">
         <MainCategory />
         <MainSeason />
--- src/components/ui/MainCarousel.tsx
+++ src/components/ui/MainCarousel.tsx
@@ -1,17 +1,90 @@
+"use client";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
+  type CarouselApi,
 } from "@/components/ui/carousel";
+import { CarouselDataType } from "@/types/main/CarouselDataType";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { PlayCarousel } from "../icons/Index";
+import Pause from "../icons/Pause";
+export default function MainCarousel({
+  carouselData,
+}: {
+  carouselData: CarouselDataType[];
+}) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+  const autoplay = useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: false, stopOnMouseEnter: true }),
+  );
+  const [isPlaying, setIsPlaying] = useState(true);
 
-export default function MainCarousel() {
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
+  const toggleAutoplay = () => {
+    if (isPlaying) {
+      autoplay.current.stop();
+      setIsPlaying(false);
+    } else {
+      autoplay.current.play();
+      setIsPlaying(true);
+    }
+  };
+
   return (
-    <Carousel className="w-[100vw]">
-      <CarouselContent className="w-full">
-        <CarouselItem>1</CarouselItem>
-        <CarouselItem>.2</CarouselItem>
-        <CarouselItem>.3.</CarouselItem>
-      </CarouselContent>
-    </Carousel>
+    <div>
+      <Carousel
+        setApi={setApi}
+        plugins={[autoplay.current]}
+        className="w-[100vw] p-0 relative"
+      >
+        <CarouselContent className="w-full">
+          {carouselData.map((item: CarouselDataType) => (
+            <CarouselItem className="relative w-[100vw] aspect-[13/10]">
+              <Image
+                src={item.imgUrl}
+                alt={item.description}
+                fill
+                style={{ objectFit: "cover" }}
+                className="p-0 w-full"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className="flex gap-[1px] absolute bottom-0 right-0 h-8 w-36 text-[13px]">
+          <div
+            className="flex items-center jutify-center bg-[rgba(0,0,0,0.45)] p-2 gap-[2px] flex-grow
+        "
+          >
+            <button className="mx-1" onClick={toggleAutoplay}>
+              {isPlaying ? <Pause /> : <PlayCarousel />}
+            </button>
+
+            <span className="text-white">{current}</span>
+            <span className="text-rgba(255,255,255,0.5)">/</span>
+            <span className="text-rgba(255,255,255,0.5)">{count}</span>
+          </div>
+          <button className="bg-[rgba(0,0,0,0.45)] p-1 text-white flex-grow">
+            전체보기
+          </button>
+        </div>
+      </Carousel>
+    </div>
   );
 }
