4e30eb422e8943809cc8124da5d4df0db5c0c463
dani001024 <aj9473@naver.com>
2024-09-11 16:23:43 +0900

    [FEAT] : checkbox 동기화

--- src/components/pages/cart/CartCheckBox.tsx
+++ src/components/pages/cart/CartCheckBox.tsx
@@ -4,14 +4,19 @@ import { Checkbox } from "@/components/ui/checkbox";
 export default function CartCheckBox({
   productCode,
   isChecked,
+  onClickCheckButton,
 }: {
   productCode: string;
   isChecked: boolean;
+  onClickCheckButton: (productCode: string) => void;
 }) {
-  const handleCheckBox = () => {
-    console.log(productCode, "상태변경 fetch");
-  };
+  //productCode로 fetch
+
   return (
-    <Checkbox checked={isChecked} onClick={handleCheckBox} className="peer" />
+    <Checkbox
+      checked={isChecked}
+      onClick={() => onClickCheckButton(productCode)} // 함수 자체를 전달
+      className="peer"
+    />
   );
 }
