e76b7fece09e1cb5a197d5b41b5b5f2d02427289
storyoser <storyoser@pukyong.ac.kr>
2024-09-13 17:05:45 +0900

    [!HOTFIX] : 빌드 에러 수정

--- src/app/buttontest/page.tsx
+++
@@ -1,9 +0,0 @@
-import ButtonGroup from "@/components/ui/ButtonGroup";
-export default function page() {
-  "use Client";
-  return (
-    <div className="w-full flex items-center">
-      <ButtonGroup></ButtonGroup>
-    </div>
-  );
-}
