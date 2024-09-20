869b60637fbb26c4e363cf78a9813be6238b776a
storyoser <storyoser@pukyong.ac.kr>
2024-09-20 10:30:21 +0900

    [FEAT] : KaKao Oauth 구현 완료

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -58,37 +58,44 @@ export const options: NextAuthOptions = {
             headers: { "Content-Type": "application/json" },
           },
         );
-        if (result.status === 400) {
+        if (result.ok) {
+          const data = await result.json();
+          user.accessToken = data.data.accessToken;
+          user.name = data.data.name;
+          return true;
+        } else if (result.status === 400) {
           console.log("400에러 발생, 존재하지 않는 회원입니다.");
           // 여기서 Kakao 정보를 `user`에 추가하여 `jwt`에서 처리할 수 있도록 넘김
           const provider = account.provider;
           const providerAccountId = account.providerAccountId;
-          console.log("dfdfd", provider, providerAccountId);
           return `/member/join?provider=${provider}&providerAccountId=${providerAccountId}`; // 회원가입 페이지로 리다이렉트
         } else {
-          console.log(result);
+          throw new Error("Kakao 로그인 중 오류 발생");
         }
       }
-      return true; // 로그인 성공
+      return true;
     },
 
-    async jwt({ token, user }) {
-      if (user) {
-        token.accessToken = user.accessToken;
-        token.name = user.name as string;
+    async jwt({ token, user, account, profile, session }) {
+      if (account && user) {
+        token.accessToken = user.accessToken; // accessToken을 string으로 캐스팅
+        token.name = user.name as string; // 이름도 string으로 캐스팅
       }
+
       return token;
     },
 
     async session({ session, token }) {
-      // token에서 Kakao 정보가 있으면 세션에 추가
       session.user = {
         ...session.user,
-        name: token.name as string,
-        accessToken: token.accessToken as string,
+        name: token.name as string, // token에서 name을 string으로 캐스팅
+        accessToken: token.accessToken as string, // token에서 accessToken을 string으로 캐스팅
       };
-
       return session;
+    },
+    async redirect({ url, baseUrl }) {
+      // 로그인 성공 후 홈("/")으로 리다이렉트
+      return "/"; // baseUrl은 기본적으로 홈("/") 경로를 가리킵니다
     },
   },
   pages: {
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,15 +4,11 @@ import "next-auth";
 declare module "next-auth" {
   interface User {
     accessToken: string; // accessToken 추가
-    provider?: string; // Kakao provider 정보 추가 (optional)
-    providerAccountId?: string; // Kakao providerAccountId 추가 (optional)
   }
 
   interface Session {
     user: {
       accessToken: string; // session.user에 accessToken 포함
-      provider?: string; // session.user에 provider 포함 (optional)
-      providerAccountId?: string; // session.user에 providerAccountId 포함 (optional)
     } & DefaultSession["user"];
   }
 }
