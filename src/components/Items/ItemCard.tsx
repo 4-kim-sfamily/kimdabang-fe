920be9be0d754820dbe41e20be2407ae100e5154
storyoser <storyoser@pukyong.ac.kr>
2024-09-26 11:38:52 +0900

    [!HOTFIX] : 배포시 발생되는 오류 주석처리

--- src/components/Items/ItemCard.tsx
+++ src/components/Items/ItemCard.tsx
@@ -4,7 +4,6 @@ import { ItemCardType } from "@/types/items/ItemCard";
 
 import Cart from "../icons/Cart";
 
-import { getCategoryName } from "@/actions/product/getCategoryName";
 import Link from "next/link";
 import ItemHearts from "../icons/ItemHearts";
 
@@ -15,7 +14,7 @@ export default async function ItemCard({
   item: ItemCardType;
   authStatus: boolean;
 }) {
-  const categoryName = await getCategoryName(item.categoryId);
+  // const categoryName = await getCategoryName(item.categoryId);
   return (
     <div className="w-[100%] border-slate-950 flex flex-col justify-start">
       <Link href={`/product/${item.productCode}`}>
@@ -29,7 +28,7 @@ export default async function ItemCard({
         </div>
       </Link>
       <div className="flex justify-between pt-1">
-        <p className="text-[#777777] text-[12px] pt-1 ">{categoryName}</p>
+        <p className="text-[#777777] text-[12px] pt-1 ">{item.categoryId}</p>
         <div className="flex gap-2">
           <ItemHearts productCode={item.productCode} authStatus={authStatus} />
           <Cart color="black" />
